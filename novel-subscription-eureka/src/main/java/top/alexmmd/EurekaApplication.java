8468fd26a04ef6be47e117c408f645f75bb31299
wangyonghui <2930807240@qq.com>
2019-10-18 16:49:05 +0800

    finish eureka server

---
+++ novel-subscription-eureka/src/main/java/top/alexmmd/EurekaApplication.java
@@ -0,0 +1,17 @@
+package top.alexmmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 汪永晖
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
