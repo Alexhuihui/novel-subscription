06715b0f1f12812ad76e3b03dd9b817ac1228649
wangyonghui <2930807240@qq.com>
2019-10-21 15:32:20 +0800

    build novel curd

--- novel-subscription-service/novel-subscription-fiction/src/main/java/top/alexmmd/service/NovelService.java
+++ novel-subscription-service/novel-subscription-fiction/src/main/java/top/alexmmd/service/NovelService.java
@@ -6,7 +6,6 @@ import top.alexmmd.domain.NovelInfo;
 /**
  * @author 汪永晖
  */
-@Service
 public interface NovelService {
 
     NovelInfo findNovelById(Long id);
---
+++ novel-subscription-service/novel-subscription-fiction/src/main/java/top/alexmmd/service/impl/NovelServiceImpl.java
@@ -0,0 +1,17 @@
+package top.alexmmd.service.impl;
+
+import org.springframework.stereotype.Service;
+import top.alexmmd.domain.NovelInfo;
+import top.alexmmd.service.NovelService;
+
+/**
+ * @author 汪永晖
+ */
+@Service
+public class NovelServiceImpl implements NovelService {
+
+    @Override
+    public NovelInfo findNovelById(Long id) {
+        return null;
+    }
+}
