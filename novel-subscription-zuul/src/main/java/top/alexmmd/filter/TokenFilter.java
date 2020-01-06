fd489db7dd7d87515f4cf2800087a150ea68d159
wangyonghui <2930807240@qq.com>
2020-01-06 11:28:39 +0800

    增加 zuul 过滤器 -> TokenFilter

--- novel-subscription-zuul/src/main/java/top/alexmmd/filter/TokenFilter.java
+++ novel-subscription-zuul/src/main/java/top/alexmmd/filter/TokenFilter.java
@@ -4,16 +4,13 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
-import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
- *
- * <h1>在过滤器中存储客户端发起请求的时间戳</h1>
  * @author 汪永晖
  */
-@Component
-public class PreRequestFilter extends ZuulFilter {
-
+public class TokenFilter extends ZuulFilter {
     @Override
     public String filterType() {
         //指定过滤器类型，由于记录时间是发生在最开始的，所以是PRE_TYPE
@@ -22,23 +19,22 @@ public class PreRequestFilter extends ZuulFilter {
 
     @Override
     public int filterOrder() {
-        //过滤器优先级，数字越小优先级越高
-        return 0;
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
     }
 
     @Override
     public boolean shouldFilter() {
-        //是否启用当前的过滤器
         return true;
     }
 
     @Override
     public Object run() throws ZuulException {
 
-        //用于在过滤器之间传递消息
-        RequestContext ctx = RequestContext.getCurrentContext();
-        ctx.set("startTime",  System.currentTimeMillis());
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
 
+        // 这里从url中参数里获取
+        request.getParameter("token");
         return null;
     }
 }
